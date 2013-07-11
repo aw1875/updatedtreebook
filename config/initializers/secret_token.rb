f9ace1cbea1d8d17bf5de10590960fe3c1e730f3
Adam Wolf <wolfynganev@gmail.com>
2013-07-10 22:27:18 -0400

    Install devise, add a user, and install devise views.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = '168f872fb6bbd78796613e8967ad420a2fe39740fc37d1aaf87b4d3d5c2168b1f2cc42ac07b882099920762a12f12fad1370c8a10451745b11841bdea4b78dcc'
