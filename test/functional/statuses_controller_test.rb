93cab6740923cd7ef259418f1edbcb6f2cbd8df8
Adam Wolf <wolfynganev@gmail.com>
2013-07-12 13:43:36 -0400

    Add status validations

--- test/functional/statuses_controller_test.rb
+++ test/functional/statuses_controller_test.rb
@@ -23,9 +23,17 @@ class StatusesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should create status" do
+  test "should be logged in to post a status" do
+    post :create, status: {content: "Hello" }
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
+  test "should create status when logged in" do
+    sign_in users(:adam)
+
     assert_difference('Status.count') do
-      post :create, status: { content: @status.content, name: @status.name }
+      post :create, status: { content: @status.content }
     end
 
     assert_redirected_to status_path(assigns(:status))
@@ -36,12 +44,26 @@ class StatusesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should get edit" do
+  test "should redirect edit when not logged in" do
+    get :edit, id: @status
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
+  test "should get edit when logged in" do
+    sign_in users(:adam)
     get :edit, id: @status
     assert_response :success
   end
 
-  test "should update status" do
+  test "should redirect status update when not logged in" do
+    put :update, id: @status, status: {content: @status.content }
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
+  test "should update status when logged in" do
+    sign_in users(:adam)
     put :update, id: @status, status: { content: @status.content }
     assert_redirected_to status_path(assigns(:status))
   end
